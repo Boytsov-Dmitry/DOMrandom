4bdf4d0d3b8a3cd656e3e8f4fef0a17dd13de3d0
Boytsov Dmitry <bdo5677905@yandex.ru>
2024-07-02 15:01:00 +0300

    second

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,31 @@
-const cellList = document.querySelectorAll(".play-cage")
-let previousCell = null;
+const cellList = document.querySelectorAll(".play-cage");
+const arr = [];
 
 
 setInterval(function () {
-    const randomCell = Math.floor(Math.random() * (cellList.length - 1));
-    const result = cellList[randomCell];
+    // const randomCell = Math.floor(Math.random() * (cellList.length - 1));
+    // const result = cellList[randomCell];
 
-    if(result !== previousCell) {
-        cellList.forEach((element) => {
-            element.classList.remove('play-cage-full')
-        })
-        result.classList.add('play-cage-full')
-        previousCell = result;
+    function getRandom(max) {
+        let randomCell = Math.floor(Math.random() * max);
+
+        while (randomCell === arr[0]) {
+            randomCell = Math.floor(Math.random() * max);
+        };
+
+        arr[0] = randomCell;
+        return randomCell;
     }
+
+    const random = getRandom(cellList.length);
+
+
+    cellList.forEach((element) => {
+        element.classList.remove('play-cage-full')
+    })
+    cellList[random].classList.add('play-cage-full')
+    // previousCell = result;
+
 }, 1000);
 
 
